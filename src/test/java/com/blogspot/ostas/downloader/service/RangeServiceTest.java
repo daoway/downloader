570b0341053ea57098413adb5133fc3606eaf55f
daoway <stas>
2023-04-24 15:45:29 +0300

    refactoring

--- src/test/java/com/blogspot/ostas/downloader/service/RangeServiceTest.java
+++ src/test/java/com/blogspot/ostas/downloader/service/RangeServiceTest.java
@@ -5,13 +5,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.blogspot.ostas.downloader.service.model.Chunk;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest(properties = {"command.line.runner.enabled=false"})
+@ExtendWith(SpringExtension.class)
 class RangeServiceTest {
 
-  @Autowired
+  @SpyBean
   private RangeService rangeService;
 
   @Test
