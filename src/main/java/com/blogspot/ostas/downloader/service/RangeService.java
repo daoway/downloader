0a86e84598dc1a4f68ba0a0483cb09f71d644ece
Stas Ostapenko <stas.ostapenko@gmail.com>
2026-01-13 17:17:49 +0200

    upgrade to jdk25

--- src/main/java/com/blogspot/ostas/downloader/service/RangeService.java
+++ src/main/java/com/blogspot/ostas/downloader/service/RangeService.java
@@ -1,23 +1,29 @@
 package com.blogspot.ostas.downloader.service;
 
 import com.blogspot.ostas.downloader.service.model.Chunk;
-import com.google.common.math.LongMath;
+import org.springframework.stereotype.Component;
+
 import java.util.LinkedHashSet;
 import java.util.Set;
-import org.springframework.stereotype.Component;
 
 @Component
 public class RangeService {
 
-  public Set<Chunk> rangeIntervals(long value, int numberOfChunks) {
-    var chunkSize = (int) Math.ceil((double) value / numberOfChunks);
-    var chunks = new LinkedHashSet<Chunk>(numberOfChunks);
-    for (int i = 0; i < numberOfChunks; i++) {
-      long start = LongMath.checkedMultiply(i, chunkSize);
-      long end = Math.min(start + chunkSize - 1, value - 1);
-      chunks.add(new Chunk(start, end, i));
+    public Set<Chunk> rangeIntervals(long value, int numberOfChunks) {
+        if (value <= 0 || numberOfChunks <= 0) {
+            throw new IllegalArgumentException("Invalid range");
+        }
+        var chunkSize = (long) Math.ceil((double) value / numberOfChunks);
+        Set<Chunk> chunks = LinkedHashSet.newLinkedHashSet(numberOfChunks);
+        long start;
+        long end;
+        for (int i = 0; i < numberOfChunks; i++) {
+            start = i * chunkSize;
+            if (start >= value) break;
+            end = Math.min(start + chunkSize - 1, value - 1);
+            chunks.add(Chunk.of(start, end, i));
+        }
+        return chunks;
     }
-    return chunks;
-  }
 
 }
