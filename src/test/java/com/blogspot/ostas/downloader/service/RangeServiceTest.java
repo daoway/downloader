573c70f1be61ac8ed282f92e55ec941516dcb18f
Stas Ostapenko <stas.ostapenko@gmail.com>
2026-01-15 00:31:47 +0200

    upgrade to jdk25

--- src/test/java/com/blogspot/ostas/downloader/service/RangeServiceTest.java
+++ src/test/java/com/blogspot/ostas/downloader/service/RangeServiceTest.java
@@ -2,68 +2,71 @@ package com.blogspot.ostas.downloader.service;
 
 import com.blogspot.ostas.downloader.service.model.Chunk;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 class RangeServiceTest {
 
-  private final RangeService rangeService  = Mockito.spy(new RangeService());
+    @Autowired
+    private RangeService rangeService;
 
-  @Test
-  void divideIntervals() {
-    int value = 10;
-    int numberOfChunks = 3;
-    var chunks = rangeService.rangeIntervals(value, numberOfChunks);
+    @Test
+    void divideIntervals() {
+        int value = 10;
+        int numberOfChunks = 3;
+        var chunks = rangeService.rangeIntervals(value, numberOfChunks);
 
-    assertThat(chunks).hasSize(numberOfChunks);
+        assertThat(chunks).hasSize(numberOfChunks);
 
-    var expected = Set.of(Chunk.of(0, 3, 0), Chunk.of(4, 7, 1), Chunk.of(8, 9, 2));
-    assertThat(chunks).isEqualTo(expected);
-  }
+        var expected = Set.of(Chunk.of(0, 3, 0), Chunk.of(4, 7, 1), Chunk.of(8, 9, 2));
+        assertThat(chunks).isEqualTo(expected);
+    }
 
-  @Test
-  void divideIntervals_1() {
-    int value = 11;
-    int numberOfChunks = 3;
+    @Test
+    void divideIntervals_1() {
+        int value = 11;
+        int numberOfChunks = 3;
 
-    var chunks = rangeService.rangeIntervals(value, numberOfChunks);
+        var chunks = rangeService.rangeIntervals(value, numberOfChunks);
 
-    assertThat(chunks).hasSize(numberOfChunks)
-        .containsExactly(Chunk.of(0, 3, 0), Chunk.of(4, 7, 1), Chunk.of(8, 10, 2));
-  }
+        assertThat(chunks).hasSize(numberOfChunks)
+                .containsExactly(Chunk.of(0, 3, 0), Chunk.of(4, 7, 1), Chunk.of(8, 10, 2));
+    }
 
-  @Test
-  void divideIntervals_2() {
-    int value = 12;
-    int numberOfChunks = 3;
-    var chunks = rangeService.rangeIntervals(value, numberOfChunks);
-    assertThat(chunks).hasSize(numberOfChunks)
-        .containsExactly(Chunk.of(0, 3, 0), Chunk.of(4, 7, 1), Chunk.of(8, 11, 2));
-  }
+    @Test
+    void divideIntervals_2() {
+        int value = 12;
+        int numberOfChunks = 3;
+        var chunks = rangeService.rangeIntervals(value, numberOfChunks);
+        assertThat(chunks).hasSize(numberOfChunks)
+                .containsExactly(Chunk.of(0, 3, 0), Chunk.of(4, 7, 1), Chunk.of(8, 11, 2));
+    }
 
-  @Test
-  void divideIntervals_big_0() {
-    long value = 10100000000L;
-    int numberOfChunks = 8;
-    var chunks = rangeService.rangeIntervals(value, numberOfChunks);
-    assertThat(chunks).hasSize(numberOfChunks);
-    var brokenRanges =
-        chunks.stream().filter(chunk -> chunk.start() < 0 || chunk.end() < 0).toList();
-    assertThat(brokenRanges).isEmpty();
-  }
+    @Test
+    void divideIntervals_big_0() {
+        long value = 10100000000L;
+        int numberOfChunks = 8;
+        var chunks = rangeService.rangeIntervals(value, numberOfChunks);
+        assertThat(chunks).hasSize(numberOfChunks);
+        var brokenRanges =
+                chunks.stream().filter(chunk -> chunk.start() < 0 || chunk.end() < 0).toList();
+        assertThat(brokenRanges).isEmpty();
+    }
 
-  @Test
-  void divideIntervals_big_1() {
-    long value = Long.MAX_VALUE - 1;
-    int numberOfChunks = 64;
-    var chunks = rangeService.rangeIntervals(value, numberOfChunks);
-    assertThat(chunks).hasSize(numberOfChunks);
-    var brokenRanges =
-        chunks.stream().filter(chunk -> chunk.start() < 0 || chunk.end() < 0).toList();
-    assertThat(brokenRanges).isEmpty();
-  }
+    @Test
+    void divideIntervals_big_1() {
+        long value = Long.MAX_VALUE - 1;
+        int numberOfChunks = 64;
+        var chunks = rangeService.rangeIntervals(value, numberOfChunks);
+        assertThat(chunks).hasSize(numberOfChunks);
+        var brokenRanges =
+                chunks.stream().filter(chunk -> chunk.start() < 0 || chunk.end() < 0).toList();
+        assertThat(brokenRanges).isEmpty();
+    }
 
 }
